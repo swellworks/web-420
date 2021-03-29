41dee61498318951d305c575c84a94bf8c8b7758
Your Name <you@example.com>
2021-03-28 20:48:12 -0500

    commit changes to the files & structure

--- api-gateway/config.js
+++ api-gateway/config.js
@@ -2,3 +2,5 @@ var config = {};
 config.web = {};
 config.web.port = process.env.PORT || '3000';
 module.exports = config;
+
+config.web.secret = ‘topsecret’
---
+++ api-gateway/models/user.js
@@ -0,0 +1,23 @@
+/*
+;============================================
+; Title:  user.js
+; Author: Perry Fulfs
+; Date:   28 March 2021
+; Description: api-gateway
+;===========================================
+
+/**
+Fields username, password, and email
+*/
+
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    email: String
+});
+
+module.exports = mongoose.model('User', userSchema);
+
+
